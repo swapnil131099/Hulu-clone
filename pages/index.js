cd89398f2f2b51b3947dd3effe2d5f0eae81d820
Swapnil <swapnilpal99@gmail.com>
2021-11-23 21:27:26 +0530

    intial commit

---
+++ pages/index.js
@@ -0,0 +1,29 @@
+import Header from "../components/Header";
+import Nav from "../components/Nav";
+import Results from "../components/Results";
+import requests from "../utils/requests";
+
+export default function Home({ results }) {
+  return (
+    <>
+      <Header />
+      <Nav />
+      <Results results={results} />
+    </>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchTrending.url
+    }`
+  ).then((res) => res.json());
+
+  return {
+    props: {
+      results: request.results,
+    },
+  };
+}
